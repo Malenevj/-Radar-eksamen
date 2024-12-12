12f49c0dff6ac85efa6e19fafd9ac6232d5506da
Malene <eaa24mvjo@students.eaaa.dk>
2024-12-12 18:20:48 +0100

    opdatere version i forhold til navigation på mobil

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,28 @@ document.getElementById('burger-menu').addEventListener('click', function () {
 });
 
 
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    const navItems = document.querySelectorAll('.nav-item');
+  
+    navItems.forEach((item) => {
+      const dropdown = item.querySelector('.dropdown');
+      const link = item.querySelector('a');
+  
+      link.addEventListener('click', (e) => {
+        if (window.innerWidth < 768) { // Mobilvisning
+          e.preventDefault(); // Forhindrer navigation på mobil
+          const openDropdown = document.querySelector('.dropdown.active');
+  
+          // Luk andre åbne dropdowns
+          if (openDropdown && openDropdown !== dropdown) {
+            openDropdown.classList.remove('active');
+          }
+  
+          // Toggler dropdown
+          dropdown.classList.toggle('active');
+        }
+      });
+    });
+  });
+  
 
